24efcd686df7ce9e18a60a88f1c40a46d4a92991
Sawant kumar <sawakumar@deloitte.com>
2017-04-14 12:09:10 +0530

    initial

--- const.js
+++ const.js
@@ -9,7 +9,7 @@ if (!WIT_TOKEN) {
 // Messenger API parameters
 const FB_PAGE_TOKEN = "EAAJbdXC4E9kBAI3AlQbzScNuCkfhZBZCdoDy6VIVX5VP8m1pBA56xolOWS7RqtQVZCjC9DljgoA9FqdVdgmqALfxYCrqgE6GXJIR7NOxiJOcgK4ZALeeQTZCDo0KweiDhZANT8qcgyMFduJdNzFMe001HMhkHPt6wiOlmZBYq8xZAvjvU1ZC5cRpe";//process.env.FB_PAGE_TOKEN;
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+var FB_VERIFY_TOKEN = "my_voice_is_my_password_verify_me";process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
   FB_VERIFY_TOKEN = "just_do_it";
 }
