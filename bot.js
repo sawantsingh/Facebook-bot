1ba6027a12a56792232eb214af8682641c2935b9
Sawant kumar <sawakumar@deloitte.com>
2017-04-16 17:14:23 +0530

    ok

--- bot.js
+++ bot.js
@@ -55,30 +55,30 @@ const actions = {
     }
   },
 
-  // ['merge'](sessionId, context, entities, message, cb) {
-  //   // Retrieve the location entity and store it into a context field
-  //   const loc = firstEntityValue(entities, 'location');
-  //   if (loc) {
-  //     console.log('Location found', loc);
-  //     context.loc = loc; // store it in context
-  //   }
-  //   cb(context);
-  // },
+  ['merge'](sessionId, context, entities, message, cb) {
+    // Retrieve the location entity and store it into a context field
+    const loc = firstEntityValue(entities, 'location');
+    if (loc) {
+      console.log('Location found', loc);
+      context.loc = loc; // store it in context
+    }
+    cb(context);
+  },
 
-  // error(sessionId, context, error) {
-  //   console.log(error.message);
-  // },
+  error(sessionId, context, error) {
+    console.log(error.message);
+  },
 
-  // // fetch-weather bot executes
-  // ['fetch-weather'](sessionId, context, cb) {
-  //   // Here should go the api call, e.g.:
-  //   // context.forecast = apiCall(context.loc)
+  // fetch-weather bot executes
+  ['fetch-weather'](sessionId, context, cb) {
+    // Here should go the api call, e.g.:
+    // context.forecast = apiCall(context.loc)
 
-  //   context.forecast = 'sunny';
-  //   cb(context);
-  // },
+    context.forecast = 'sunny';
+    cb(context);
+  },
 
-
+/*
   ['getForecast'](sessionId, context, entities, message, cb ) {
     var location = firstEntityValue(entities, "location")
     if (location) {
@@ -90,6 +90,7 @@ const actions = {
     }
     cb(context);
 },
+*/
 
 
 };
