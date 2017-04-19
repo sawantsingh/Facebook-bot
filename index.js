f308325fd972fe1db36f5e81993bb17cf84b3cd6
Sawant kumar <sawakumar@deloitte.com>
2017-04-19 10:17:52 +0530

    ok

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.post('/webhook/', function (req, res) {
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200))
 			continue
 		}
 	}
