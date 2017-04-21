1ef3ad57d28633a6e83c79d3a84c0f95b1d8e3f2
Sawant kumar <sawakumar@deloitte.com>
2017-04-21 07:37:08 +0530

    ok

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 //
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request')
+
 
 // get Bot, const, and Facebook API
 const bot = require('./bot.js');
@@ -74,13 +76,46 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-// The main message handler
-app.post('/webhook', (req, res) => {
-  // Parsing the Messenger API response
-  const messaging = FB.getFirstMessagingEntry(req.body);
-  if (messaging && messaging.message) {
 
-    // Yay! We got a new message!
+/*
+app.get('/setup',function(req,res){
+
+    //setupGetStartedButton(res);
+    var messageData = {
+                "get_started":[
+                {
+                    "payload":"Welcome to My Car Insurance"
+                    }
+                ]
+        };
+
+        // Start the request
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ Config.FB_PAGE_TOKEN,
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            form: messageData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                res.send(body);
+
+            } else { 
+                // TODO: Handle errors
+                res.send(body);
+            }
+        });
+});
+*/
+
+
+app.post('/webhook/', function (req, res) {
+	
+	console.log("Main console" + req);
+
+  const messaging = FB.getFirstMessagingEntry(req.body);
+  // Yay! We got a new message!
 
     // We retrieve the Facebook user ID of the sender
     const sender = messaging.sender.id;
@@ -88,6 +123,8 @@ app.post('/webhook', (req, res) => {
     // We retrieve the user's current session, or create one if it doesn't exist
     // This is needed for our bot to figure out the conversation history
     const sessionId = findOrCreateSession(sender);
+
+  if (messaging && messaging.message) {
 
     // We retrieve the message content
     const msg = messaging.message.text;
@@ -133,5 +170,296 @@ app.post('/webhook', (req, res) => {
       );
     }
   }
+  else if (event.postback) {
+			let text1 = JSON.stringify(event.postback.payload)
+      console.log( "message text 1" + text1)
+
+			//sendTextMessage(sender, "Postback received: "+text.substring(0, 200))
+      if (text1 === '"Start Chatting"') {
+         {
+            //const {text, attachments} = event.message;
+          
+           sendTextMessage(sender, 'Hello there!')  
+
+          }
+        }
+      else {
+            sendWelcomeMessage(sender)
+      }
+		}
+
+/*
+	let messaging_events = req.body.entry[0].messaging
+
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+    const sessionId = findOrCreateSession(sender);
+
+  	if (event.message && event.message.text) {
+			let text = event.message.text
+       console.log( "Main text" + text)
+
+      //  
+      sendTextMessage(sender, text);
+
+        wit.runActions(
+              sessionId, // the user's current session
+              text, // the user's message
+              sessions[sessionId].context // the user's current session state
+            ).then((context) => {
+            
+              console.log('Waiting for next user messages');
+
+              // if (context['done']) {
+              //   delete sessions[sessionId];
+              // }
+
+              sessions[sessionId].context = context;
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })	
+            
+		}   
+  else if (event.postback) {
+			let text1 = JSON.stringify(event.postback.payload)
+      console.log( "message text 1" + text1)
+
+			//sendTextMessage(sender, "Postback received: "+text.substring(0, 200))
+      if (text1 === '"Start Chatting"') {
+         {
+            //const {text, attachments} = event.message;
+          
+           sendTextMessage(sender, 'Hello there!')  
+
+          }
+        }
+      else {
+            sendWelcomeMessage(sender)
+      }
+		}
+	}
+  */
+	res.sendStatus(200)
+})
+
+
+/*
+// The main message handler
+app.post('/webhook', (req, res) => {
+   // Parse the Messenger payload
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const data = req.body;
+
+  if (data.object === 'page') {
+    data.entry.forEach(entry => {
+      entry.messaging.forEach(event => {
+
+        if(event.postback) {
+            res.send("Get Started called");
+        }
+
+
+        if (event.message && !event.message.is_echo) {
+          // Yay! We got a new message!
+          // We retrieve the Facebook user ID of the sender
+          const sender = event.sender.id;
+
+          // We retrieve the user's current session, or create one if it doesn't exist
+          // This is needed for our bot to figure out the conversation history
+          const sessionId = findOrCreateSession(sender);
+
+          // We retrieve the message content
+          const {text, attachments} = event.message;
+
+          if (attachments) {
+            // We received an attachment
+            // Let's reply with an automatic message
+            fbMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
+          } else if (text) {
+            // We received a text message
+
+            // Let's forward the message to the Wit.ai Bot Engine
+            // This will run all actions until our bot has nothing left to do
+            wit.runActions(
+              sessionId, // the user's current session
+              text, // the user's message
+              sessions[sessionId].context // the user's current session state
+            ).then((context) => {
+              // Our bot did everything it has to do.
+              // Now it's waiting for further messages to proceed.
+              console.log('Waiting for next user messages');
+
+              // Based on the session state, you might want to reset the session.
+              // This depends heavily on the business logic of your bot.
+              // Example:
+              // if (context['done']) {
+              //   delete sessions[sessionId];
+              // }
+
+              // Updating the user's current session state
+              sessions[sessionId].context = context;
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })
+          }
+        } else {
+          console.log('received event', JSON.stringify(event));
+        }
+      });
+    });
+  }
   res.sendStatus(200);
 });
+
+*/
+
+
+function sendResponseData(sender,response) {
+
+	 var json = JSON.parse(response.body);
+		console.log('Printing json:',json)
+
+		let hourly = json["hourly"]
+		let summary = hourly["summary"]
+		console.log('Printing json:',summary)
+
+
+	let messageData = { text:summary }
+
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:Config.FB_PAGE_TOKEN},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+	
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:Config.FB_PAGE_TOKEN},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+function sendGenericMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "https://scontent-sin6-1.xx.fbcdn.net/v/t31.0-8/17218524_10211278521054605_3920462141316408947_o.jpg?oh=8307dfaf91be1b3608dc99327f12fecb&oe=59971EED",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://en.wikipedia.org/wiki/Deloitte",
+						"title": "Deloitte"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://www.qspiders.com/sites/default/files/sunny%20amar%20nath.jpg",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.allianz.com/en/",
+						"title": "Allianz"
+					},{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:Config.FB_PAGE_TOKEN},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+
+function sendWelcomeMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "Welcome to My Car Insurance",
+					"subtitle": "You’re In Good Hands",
+					"image_url": "https://www.aautoandhomeinsurance.com/img/~www.aautoandhomeinsurance.com/cheapest%20auto%20pics/17102035437_f005f23cb7_b.jpg",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.allianz.com/en/",
+						"title": "Visit our website"
+					},{
+						"type": "postback",
+						"title": "Start Chatting",
+						"payload": "Start Chatting",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:Config.FB_PAGE_TOKEN},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
