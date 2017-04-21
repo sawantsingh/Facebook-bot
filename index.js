5fa638407a5fabada8fed82975f12fe371761582
Sawant kumar <sawakumar@deloitte.com>
2017-04-21 08:35:51 +0530

    ok

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.post('/webhook/', function (req, res) {
       );
     }
   }
-  else if (messaging.postback) {
+  else if (event.postback) {
 			let text1 = JSON.stringify(event.postback.payload)
       console.log( "message text 1" + text1)
 
