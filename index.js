e15cdd62e2e74fb6fd8f08ef165f99e10c195b1b
Sawant kumar <sawakumar@deloitte.com>
2017-04-21 06:35:57 +0530

    ok

--- index.js
+++ index.js
@@ -125,8 +125,9 @@ app.post('/webhook/', function (req, res) {
 			let text = event.message.text
        console.log( "Main text" + text)
 
-      //  sendTextMessage(sender, text);
-
+      //  
+      sendTextMessage(sender, text);
+/*
         wit.runActions(
               sessionId, // the user's current session
               text, // the user's message
@@ -144,6 +145,7 @@ app.post('/webhook/', function (req, res) {
             .catch((err) => {
               console.error('Oops! Got an error from Wit: ', err.stack || err);
             })	
+            */
 		}   
   else if (event.postback) {
 			let text1 = JSON.stringify(event.postback.payload)
